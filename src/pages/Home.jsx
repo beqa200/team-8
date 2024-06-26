939dcb7e16bfb4545fcefee075991cb752c06b0d
unknown <nikolozgelenidze9@gmail.com>
2024-06-26 15:26:32 +0400

    add navigation on button click using useNavigate hook

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import { useNavigate } from "react-router-dom";
 import redBerryIcon from "/images/LOGO-02 3.png";
 import backgroundLogo from "/images/LOGO-40 1.png";
 import backGroundImg from "/images/shutterstock_2189773743 1.png";
+useNavigate;
+
 export default function Home() {
+  const navigate = useNavigate();
   return (
     <>
       <section
@@ -15,21 +19,26 @@ export default function Home() {
             className="mb-[26px]"
           />
         </header>
-        <div className="border-b border-[#1A1A1A] ml-[70px] mr-[70px]"></div>
-        <div className="flex items-center justify-center h-[100vh] relative">
-          <div>
-            <img
-              src={backgroundLogo}
-              alt="backgroundLogoImg"
-              className="absolute left-[56%] top-[47%]"
-            />
+        <hr className="border-[#1A1A1A] ml-[70px] mr-[70px]"></hr>
+        <main>
+          <div className="flex items-center justify-center h-[100vh] relative">
             <div>
-              <button className="bg-black text-white w-[464px] p-[18px_60px] rounded-lg text-xl">
-                რეზიუმეს დამატება
-              </button>
+              <img
+                src={backgroundLogo}
+                alt="backgroundLogoImg"
+                className="absolute left-[56%] top-[47%]"
+              />
+              <div>
+                <button
+                  onClick={() => navigate("/personal")}
+                  className="bg-black text-white w-[464px] p-[18px_60px] rounded-lg text-xl"
+                >
+                  რეზიუმეს დამატება
+                </button>
+              </div>
             </div>
           </div>
-        </div>
+        </main>
       </section>
     </>
   );
